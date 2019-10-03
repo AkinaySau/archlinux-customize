378e72ed98c499508349b608d79c1440ecde1ae0
sau <akinaysau@gmail.com>
2019-10-03 10:28:11 +0300

    update awesome

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -7,8 +7,13 @@ local dpi = require("beautiful.xresources").apply_dpi
 local theme = dofile(themes_path .. "gtk/theme.lua")
 local path = '~/.sau/awesome/'
 
-theme.wallpaper = path .. '/image/wallpaper.jpg'
-theme.border_width = dpi(2)
-theme.useless_gap = dpi(3)
+local font_size = 10
+
+theme.font			= theme.gtk.font_family .. ' ' .. font_size
+theme.gtk.bold_font = theme.gtk.font_family .. ' Bold ' .. font_size
+
+theme.wallpaper		= path .. '/image/wallpaper.jpg'
+theme.border_width	= dpi(3)
+theme.useless_gap	= dpi(1)
 
 return theme
